6beb2542c36950057b0e0d4a74d268fba473415d
Caroline Portela <110353130+carolineportela@users.noreply.github.com>
2025-07-06 21:57:14 -0300

    Update DefaultProjectApplicationTests.java

--- src/test/java/br/portela/startuplogistica/DefaultProjectApplicationTests.java
+++ src/test/java/br/portela/startuplogistica/DefaultProjectApplicationTests.java
@@ -1,14 +1,21 @@
-// src/test/java/br/portela/startuplogistica/DefaultProjectApplicationTests.java
 package br.portela.startuplogistica;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DefaultProjectApplicationTests {
+
+    // injeta um JavaMailSender vazio para satisfazer o SmtpEmailService
+    @MockBean
+    private JavaMailSender javaMailSender;
 
     @Test
     void contextLoads() {
-        // se o Spring levantar o ApplicationContext com H2 e sem erros, passa.
+        // agora o Spring sobe o contexto sem reclamar do JavaMailSender
     }
 }
